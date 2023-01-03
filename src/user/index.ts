8b29b23c09aaf573a4d73ff2e4e248895e892a47
Aleksandr Levus <aleksandr.l@ati.su>
2023-01-03 21:45:03 +0800

    Remove UserRepository from index.ts reexport

--- src/user/index.ts
+++ src/user/index.ts
@@ -1,4 +1,3 @@
 export { default as UserController } from './user.controller';
-export { default as UserRepository } from './user.repository';
 export { default as UserService } from './user.service';
 export { default as User } from './user.entity';
